2dc2dfeb86411ecc084098cfa7fa026d2d0d8de4
Quanmeo <huuquan.learn@gmail.com>
2023-03-27 23:22:00 +0700

    Add button controller and choose number of rectangle from input range

--- script.js
+++ script.js
@@ -1,17 +1,81 @@
-const container = document.querySelector('.container');
+const defaultColor = '#000000';
+window.addEventListener('load', startUp, false);
 
-const number = 16;
+let colorChoosen = null;
+let numberChoosen = 30;
 
-for (let i = 0; i < number; i++) {
-    const divRow = document.createElement('div');
-    divRow.setAttribute('class', `row ${i}`);
+function drawGrid(number) {
 
-    for (let j = 0; j < number; j++) {
-        const div = document.createElement('div');
-        div.setAttribute('class', `column ${j}`);
-
-        divRow.appendChild(div);
+    const container = document.querySelector('.container');
+    
+    const size = 600;
+    
+    const border = 0;
+    
+    for (let i = 0; i < number; i++) {
+        const divs = document.createElement('div');
+        divs.classList.add('row');
+    
+        for (let j = 0; j < number; j++) {
+            const div = document.createElement('div');
+            div.classList.add('rectangle');
+    
+            const divSize = (size - number * 2 * border) / number;
+            div.style.width = `${divSize}px`;
+            div.style.height = `${divSize}px`;
+    
+            divs.appendChild(div);
+        }
+    
+        container.appendChild(divs);
     }
 
-    container.appendChild(divRow);
 }
+
+function addBackground(e) {
+    this.style.backgroundColor = colorChoosen;
+}
+
+function removeBackground(e) {
+    this.style.backgroundColor = null;
+}
+
+function addEvent() {
+    const divs = document.querySelectorAll('.rectangle');
+    
+    divs.forEach(div => div.addEventListener('mouseover', addBackground));
+    
+    divs.forEach(div => div.addEventListener('mouseleave', removeBackground));
+}
+
+function game() {
+    drawGrid(numberChoosen);
+
+    addEvent();
+}
+
+function setColorPicker(color) {
+
+    const input = document.querySelector('#colorPicker');
+
+    input.style.backgroundColor = color;
+}
+function updateFirst(event) {
+    const color = event.target.value;
+
+    setColorPicker(color);
+}
+
+function updateAll(event) {
+    colorChoosen = event.target.value;
+}
+
+function startUp() {
+    colorChoosen = defaultColor;
+    setColorPicker(defaultColor);
+    const colorPicker = document.querySelector('#colorPicker');
+    colorPicker.addEventListener('input', updateFirst, false);
+    colorPicker.addEventListener('change', updateAll, false);
+}
+
+game();
