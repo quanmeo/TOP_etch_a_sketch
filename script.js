16e8fd4ff4a83b8c6f32406a482313e22f75bb2a
Quanmeo <huuquan.learn@gmail.com>
2023-03-28 23:04:19 +0700

    Finalize

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const defaultColor = '#000000';
+const defaultColor = '#ffff00';
 window.addEventListener('load', startUp, false);
 
 let colorChoosen = null;
 let numberChoosen = 30;
+let rainBow = false;
+
+let idx = 0;
+const rainBowSize = 7;
+const rainBowList = ['#9400D3', '#4B0082', '#0000FF', '#00FF00',
+                    '#FFFF00', '#FF7F00', '#FF0000'];
 
 function drawGrid(number) {
 
@@ -19,6 +25,8 @@ function drawGrid(number) {
         for (let j = 0; j < number; j++) {
             const div = document.createElement('div');
             div.classList.add('rectangle');
+
+            div.setAttribute('id', `${i}|${j}`);
     
             const divSize = (size - number * 2 * border) / number;
             div.style.width = `${divSize}px`;
@@ -32,23 +40,45 @@ function drawGrid(number) {
 
 }
 
+function chooseColor() {
+    if (!rainBow) {
+        return colorChoosen;
+    } else {
+        idx = (idx + 1) % rainBowSize;
+        return rainBowList[idx];
+    }
+}
+
 function addBackground(e) {
-    this.style.backgroundColor = colorChoosen;
+    this.style.backgroundColor = chooseColor();
 }
 
 function removeBackground(e) {
-    this.style.backgroundColor = null;
+    e.style.backgroundColor = null;
+}
+
+function clearBackgroundColor(e) {
+    const container = document.querySelector('.container');
+    if (container) {
+        const rows = container.childNodes;
+
+        rows.forEach((r) => {
+            const recs = r.childNodes;
+            recs.forEach(removeBackground);
+        });
+    }
+    setRainBow(false);
 }
 
 function addEvent() {
     const divs = document.querySelectorAll('.rectangle');
     
-    divs.forEach(div => div.addEventListener('mouseover', addBackground));
-    
-    divs.forEach(div => div.addEventListener('mouseleave', removeBackground));
+    divs.forEach(div => div.addEventListener('mouseover', addBackground));    
 }
 
 function game() {
+    removeAllChildrenGrid();
+
     drawGrid(numberChoosen);
 
     addEvent();
@@ -66,8 +96,51 @@ function updateFirst(event) {
     setColorPicker(color);
 }
 
+function updateColorRainbowBtn() {
+    const rainBowBtn = document.querySelector('#rainbow');
+    if (rainBow) {
+        rainBowBtn.style.backgroundColor = '#FF7F00';
+    } else {
+        rainBowBtn.style.backgroundColor = null;
+    }
+}
+
+function setRainBow(state) {
+    rainBow = state;
+    idx = 0;
+    updateColorRainbowBtn();
+}
+
 function updateAll(event) {
     colorChoosen = event.target.value;
+    rainBow = false;
+
+    setRainBow(false);
+}
+
+function removeAllChildrenGrid() {
+    const container = document.querySelector('.container');
+
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function updateNumberOfRectangle(event) {
+    if (numberChoosen === event.target.value) {
+        return;
+    }
+
+    numberChoosen = event.target.value;
+
+    const number = document.querySelector('#numberRectangle');
+    number.textContent = numberChoosen;
+
+    game();
+}
+
+function updateRainbow() {
+    setRainBow(!rainBow);
 }
 
 function startUp() {
@@ -76,6 +149,15 @@ function startUp() {
     const colorPicker = document.querySelector('#colorPicker');
     colorPicker.addEventListener('input', updateFirst, false);
     colorPicker.addEventListener('change', updateAll, false);
+    
+    const input = document.querySelector('input[type=range]');
+    input.addEventListener('input', updateNumberOfRectangle);
+
+    const clearBtn = document.querySelector('#clear');
+    clearBtn.addEventListener('click', clearBackgroundColor);
+
+    const rainBowBtn = document.querySelector('#rainbow');
+    rainBowBtn.addEventListener('click', updateRainbow);
 }
 
 game();
